1636cf7425c3c1c2f7cd9fb9b7f5686a0d321901
Anton Valov <anton_valov@epam.com>
2016-08-09 10:15:32 +0300

    Initial commit

---
+++ src/main/java/com/chat/util/json/JsonObjectFactory.java
@@ -0,0 +1,36 @@
+package com.chat.util.json;
+
+import com.chat.util.entity.User;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.IOException;
+
+public final class JsonObjectFactory {
+    private JsonObjectFactory() {
+    }
+
+    private static ObjectMapper mapper = new ObjectMapper();
+
+    public static String getJsonString(String command, User user) throws JsonProcessingException {
+        JsonObject jsonObject = new JsonObject(command, user);
+        return mapper.writeValueAsString(jsonObject);
+    }
+
+    public static <T> String getJsonString(T object) {
+        try {
+            return mapper.writeValueAsString(object);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+            return "";
+        }
+    }
+
+    public static <T> T getObjectFromJson(String json, Class<T> tClass) {
+        try {
+            return mapper.readValue(json, tClass);
+        } catch (IOException e) {
+            return null;
+        }
+    }
+}
